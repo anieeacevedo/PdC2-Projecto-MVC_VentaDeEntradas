b12b4d8e6737ae5b9114bcc482c8bfb16eb3e148
aniee <aniee>
2026-05-22 11:59:05 -0500

    Update - Entrada

---
+++ VentaDeEntradas_V2/src/main/java/Modelo/Entrada.java
@@ -0,0 +1,14 @@
+package Modelo;
+
+public class Entrada {
+    private int numero;
+    private String estado;
+
+    public boolean vender() {
+        return false;
+    }
+
+    public boolean liberar() {
+        return false;
+    }
+}
