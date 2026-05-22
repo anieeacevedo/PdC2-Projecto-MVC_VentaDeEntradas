13dfd7b7181bce4a6fe91776509530f4c492acd7
aniee <aniee>
2026-05-22 11:59:46 -0500

    Update - Tarjeta

---
+++ VentaDeEntradas_V2/src/main/java/Modelo/Tarjeta.java
@@ -0,0 +1,13 @@
+package Modelo;
+
+public class Tarjeta {
+    private int numero;
+    private String nombre;
+    private String fecha;
+    private int CVV;
+
+    // En el diagrama figura "Type", lo mapeamos como un Object genérico
+    public Object method() {
+        return null;
+    }
+}
