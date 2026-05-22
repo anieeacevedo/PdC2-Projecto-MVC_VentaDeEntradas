da62e8ac8decb68a6a972764e018ee36d27356cf
aniee <aniee>
2026-05-22 12:57:39 -0500

    Update - Ventaaa

--- VentaDeEntradas_V2/src/main/java/Modelo/Venta.java
+++ VentaDeEntradas_V2/src/main/java/Modelo/Venta.java
@@ -7,13 +7,27 @@ import java.util.ArrayList;
 public class Venta {
     private Date fecha;
     private int monto;
-    private List<Entrada> entradas; // Multiplicidad 1,4 (Se controla en la lógica empresarial)
+    private List<Entrada> entradas;
 
     public Venta() {
         this.entradas = new ArrayList<>();
     }
-
     public boolean anular() {
         return false;
+    }
+    public List<Entrada> getEntradas() {
+        return entradas;
+    }
+    public void setMonto(int monto) {
+        this.monto = monto;
+    }
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+    public Date getFecha() {
+        return fecha;
+    }
+    public int getMonto() {
+        return monto;
     }
 }
